11092be376617ec130eb846c00c38e37dcf14b97
Seiyeon Joo <dorothy01427@naver.com>
2023-07-10 23:51:51 +0900

    fe

--- frontend/프론트 통합본/job/src/App.js
+++ frontend/프론트 통합본/job/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import HomePage from "./pages/Home";
-import DetailPage from "./pages/Detail";
+import SearchDetailPage from "./pages/SearchDetail";
+import ShowDetailPage from "./pages/ShowDetail";
 import PostPage from "./pages/Post";
 import SignupPage from "./pages/Signup";
 import LoginPage from "./pages/Login";
+import ResumePage from "./pages/Resume";
 
 function App(){
     return (     
       <Routes>
         <Route path="/" element = {<HomePage/>} />
-        <Route path="/detail" element = {<DetailPage/>} />
+        <Route path="/show_detail" element = {<ShowDetailPage/>} />
+        <Route path="/search_detail" element = {<SearchDetailPage/>} />
         <Route path="/post" element = {<PostPage/>} />        
         <Route path="/signup" element = {<SignupPage/>} />
-        <Route path="/login" element = {<LoginPage/>} />        
+        <Route path="/login" element = {<LoginPage/>} />    
+        <Route path="/resume" element={<ResumePage/>}></Route>    
         <Route path="*" element={<div>404 NotFound</div>}/>
       </Routes>    
     );
